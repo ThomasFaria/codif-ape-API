8cd12110b7de96000fe3519cb9e9de65389a3b45
tfaria <thomas.faria@insee.fr>
2023-04-04 15:23:29 +0000

    add welcome page

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 import os
-import warnings
-from typing import List
 
-import joblib
-import mlflow
-import pandas as pd
 from fastapi import FastAPI
 
 codification_ape_app = FastAPI()
 
 
-@codification_ape_app.get("/hello")
-async def hello():
-    return {"Message": "coucou"}
+@codification_ape_app.get("/")
+def show_welcome_page():
+    model_name: str = os.getenv("MLFLOW_MODEL_NAME")
+    model_version: str = os.getenv("MLFLOW_MODEL_VERSION")
+    return {
+        "Message": "Codification de l'APE",
+        "Model_name": f"{model_name}",
+        "Model_version": f"{model_version}",
+    }
+
