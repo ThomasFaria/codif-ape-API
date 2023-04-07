80942022cd1e027a8862a106c0a1a69b26a74466
tfaria <thomas.faria@insee.fr>
2023-04-07 10:12:20 +0000

    add pre-commit

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 git config --global credential.helper store
 
 pip install -r requirements.txt
+pip install pre-commit
+pre-commit install
 
 AWS_ACCESS_KEY_ID=`vault kv get -field=ACCESS_KEY onyxia-kv/projet-ape/s3` && export AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY=`vault kv get -field=SECRET_KEY onyxia-kv/projet-ape/s3` && export AWS_SECRET_ACCESS_KEY
