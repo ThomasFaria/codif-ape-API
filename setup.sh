86854ce3a5139aef9d7abf3e5d615cd96f4357ba
tfaria <thomas.faria@insee.fr>
2023-04-11 16:11:37 +0000

    update mlflow uri

--- setup.sh
+++ setup.sh
@@ -10,9 +10,9 @@ AWS_SECRET_ACCESS_KEY=`vault kv get -field=SECRET_KEY onyxia-kv/projet-ape/s3` &
 unset AWS_SESSION_TOKEN
 
 export MLFLOW_S3_ENDPOINT_URL="https://$AWS_S3_ENDPOINT"
-export MLFLOW_TRACKING_URI=https://projet-ape-654210.user.lab.sspcloud.fr
+export MLFLOW_TRACKING_URI=https://projet-ape-529043.user.lab.sspcloud.fr
 export MLFLOW_MODEL_NAME=test
-export MLFLOW_MODEL_VERSION=2
+export MLFLOW_MODEL_VERSION=3
 
 python - <<'END_SCRIPT'
 import nltk
