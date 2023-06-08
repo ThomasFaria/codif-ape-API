962c51140617b318bb4aad45fc87fa632d334145
ThomasFaria <thomas.faria@ensae.fr>
2023-06-08 12:38:30 +0000

    use v0.0.7

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ AWS_SECRET_ACCESS_KEY=`vault kv get -field=SECRET_KEY onyxia-kv/projet-ape/s3` &
 unset AWS_SESSION_TOKEN
 
 export MLFLOW_S3_ENDPOINT_URL="https://$AWS_S3_ENDPOINT"
-export MLFLOW_TRACKING_URI=https://projet-ape-713513.user.lab.sspcloud.fr
+export MLFLOW_TRACKING_URI=https://projet-ape-57262.user.lab.sspcloud.fr
 export MLFLOW_MODEL_NAME=FastText-APE
 export MLFLOW_MODEL_VERSION=5
 export API_USERNAME=username
